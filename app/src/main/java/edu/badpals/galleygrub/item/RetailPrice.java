5785a777cf82e906e82fde686794a7be253bd44a
David Baquero <david.baquero.19@gmail.com>
2024-04-11 12:27:47 +0200

    🔧 feat(create proyect, RetailPrice.java) : create the proyect and add class RetailPrice

---
+++ app/src/main/java/edu/badpals/galleygrub/item/RetailPrice.java
@@ -0,0 +1,30 @@
+package edu.badpals.galleygrub.item;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RetailPrice {
+    static Map<String,Double> prices = new HashMap<String,Double>();
+
+    public RetailPrice(){
+    };
+
+    public static void init_prices(){
+        prices.put("cheese",0.25);
+        prices.put("sauce",0.50);
+        prices.put("medium",0.25);
+        prices.put("large",0.50);
+    };
+
+    public static double getPrices(String item) {
+        return prices.get(item);
+    }
+
+    public static boolean contains(String item){
+        return prices.containsKey(item);
+    }
+
+    public static void display(){
+        prices.entrySet().stream().forEach(entry -> System.out.println("\t" + entry));
+    }
+}
